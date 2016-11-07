584611d415dff6d7a62a53016512fa6a1b6dd3c2
Tayyeb <tayyeb@tsaze.com>
2016-11-07 20:37:36 +0330

    add allow upload to credential_list

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = [
 
 setup(
     name='localshop',
-    version='0.10.4',
+    version='0.10.5',
     author='Michael van Tellingen',
     author_email='michaelvantellingen@gmail.com',
     url='http://github.com/mvantellingen/localshop',
